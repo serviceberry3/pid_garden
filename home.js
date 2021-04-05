81c460749635f02f74322cc9336a8c74ff4585d1
Noah Weiner <noah.weiner@yale.edu>
2021-04-05 12:21:53 -0400

    add controller

--- home.js
+++ home.js
@@ -2,18 +2,37 @@ var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.beginPath();
 
-//draw rectangle symbolizing the garden : x, y, width, height
-ctx.rect(30, 500, 800, 400);
-ctx.fillText("My garden", 400, 800);
-ctx.stroke();
+//current fill point of garden, used for coloring
+var currentFillPt = 0;
+
+var gardenTopLeftX = 30;
+var gardenTopLeftY = 500;
+var gardenBottomRtX = 830;
+var gardenBottomRtY = 900;
+
+var gardenWidth = gardenBottomRtX - gardenTopLeftX;
+var gardenHeight = gardenBottomRtY - gardenTopLeftY;
+
+//span tag displaying current moisture value
+var moistureVal = document.getElementById("moisture_val");
+var onOffBtn = document.getElementById("onoffbtn");
+
+//desired moisture level and actual moisture level
+var desired = 0, actual = 0;
+
+var runPid = false;
 
 class PIDCtrl
 {
   constructor(p, i, d)
   {
+    console.log("Setting this.Kp to ", p);
     this.Kp = p;
     this.Ki = i;
     this.Kd = d;
+    this.compP = 0;
+    this.compI = 0;
+    this.compD = 0;
     this.ResetCtrl();
   }
   
@@ -25,37 +44,133 @@ class PIDCtrl
   
   Update(error, dt)
   {
-    integError += error * dt;
-    this.compP = error * Kp;
-    this.compI = integError * Ki;
-    this.compD = ((error - lastError) / dt) * Kd;
-    lastError = error;
-    return compP + compI + compD;
+    this.integError += error * this.dt;
+    this.compP = error * this.Kp;
+    this.compI = this.integError * this.Ki;
+    this.compD = ((error - this.lastError) / this.dt) * this.Kd;
+    this.lastError = error;
+    return this.compP + this.compI + this.compD;
   }
   
   ComponentP() {
-    return compP;
+    return this.compP;
   }
   ComponentI() {
-    return compI;
+    return this.compI;
   }
   ComponentD() {
-    return compD;
+    return this.compD;
+  }
+
+  getKp() {
+      return this.Kp;
+  }
+
+  getKi() {
+      return this.Ki;
+  }
+
+  getKd() {
+      return this.Kd;
   }
 }
 
-class TestClass {
+let ctrl = new PIDCtrl(1, 1, 1);
+
+
+
+//draw rectangle symbolizing the garden : x, y, width, height
+ctx.rect(gardenTopLeftX, gardenTopLeftY, gardenBottomRtX - gardenTopLeftX, gardenBottomRtY - gardenTopLeftY);
+
+//draw some text in the garden
+ctx.fillText("My garden", 400, 800);
+
+//draw to screen
+ctx.stroke();
+
+function addDrop() {
+    console.log("addDrop() called");
+
+    ctx.fillStyle = "#0000BB";
+    ctx.fillRect(gardenTopLeftX, gardenBottomRtY - currentFillPt, gardenWidth, 1);
+
+    let newMoistureVal = currentFillPt / gardenHeight * 100;
+    actual = currentFillPt / gardenHeight * 100;
+
+    moistureVal.innerHTML = newMoistureVal;
+
+    currentFillPt++;
+}
+
+function removeDrop() {
 
 }
 
+function setDesired() {
+    desired = document.getElementById("desired").value;
+
+    console.log("Desired is ", desired);
+
+    if (desired != actual) {
+        pidLoop();
+    }
+}
+
+
+
+class TestClass {
+    
+}
+
+//sleep (blocking) for number of milliseconds
+function sleep(milliseconds) {
+    console.log("Sleep of ", milliseconds, "ms requested");
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        //console.log("Sleeping");
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
 
 
 function pidLoop() {
+    console.log("Running pidLoop()");
 
+
+    var prevTime = Date.now();
+
+    var i = 0
+    while (actual != desired) {
+        //console.log("ComponentP() returns", ctrl.ComponentP());
+        sleep(ctrl.ComponentP());
+
+        //add drop of water to the garden
+        //requestAnimationFrame(addDrop);
+        requestAnimationFrame(addDrop);
+        //addDrop();
+        
+        timeElapsed = Date.now() - prevTime;
+        console.log("Time elapsed was", timeElapsed);
+
+        ctrl.Update(desired - (desired - actual), timeElapsed);
+
+        //get time in ms
+        prevTime = Date.now();
+        i++;
+    }
 }
 
-function testFunc() {
+function pidOnOffToggle() {
+    if (runPid) {
+        onOffBtn.innerHTML = "Turn on PID irrigation system";
+    }
+    else {
+        onOffBtn.innerHTML = "Turn off PID irrigation system";
+    }
 
+
+    runPid = !runPid;
 }
 
 function setProcessVars(p, i, d, mass, damping, maxf)
@@ -70,4 +185,8 @@ function setProcessVars(p, i, d, mass, damping, maxf)
 }
 
 
-let ctrl = new PIDCtrl();
+
+//window.onload = loop;
+
+
+
